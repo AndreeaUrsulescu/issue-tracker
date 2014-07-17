4b7c06ff4406c4e6099a0082a25b6850d5b4181a
unknown <aursulescu@EN61012.endava.net>
2014-07-17 11:24:59 +0300

    RegisterController

---
+++ src/main/java/internship/issuetracker/controller/RegisterController.java
@@ -0,0 +1,41 @@
+package internship.issuetracker.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+	
+	private UserService userService;
+	
+	private UserValidator userValidator;
+	
+	@Autowired
+	public RegisterController(UserService userService, UserValidator userValidator) {
+		this.userService = userService;
+		this.userValidator = userValidator;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String registerPage(Model model) {
+		model.addAttribute(new User());
+		return "/register";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String registerUser(@Valid User user, BindingResult bindingResult) {
+		userValidator.validate(user, bindingResult);
+		
+		if (bindingResult.hasErrors())
+			return "";
+		
+		userService.addUser(user);
+		return "redirect:/index";
+	}
+}
