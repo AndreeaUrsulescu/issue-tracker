4745cbd5c5324bf1f7cc57f55f8534da38c08043
unknown <acorbu@EN60382.endava.net>
2014-07-17 12:31:17 +0300

    update user service class

--- src/main/java/internship/issuetracker/service/UserService.java
+++ src/main/java/internship/issuetracker/service/UserService.java
@@ -1,6 +1,8 @@
 package internship.issuetracker.service;
 
 
+import java.util.List;
+
 import internship.issuetracker.entities.User;
 import internship.issuetracker.repository.UserRepository;
 
@@ -19,6 +21,22 @@ public class UserService {
 
 	public void addUser(User user){
 		this.userRepository.create(user);
+	}
+	
+	public void removeUser(User user){
+		this.userRepository.remove(user);
+	}
+	
+	public void updateUser(User user){
+		this.userRepository.update(user);
+	}
+	
+	public User findUser(Long id){
+		return this.userRepository.find(id);
+	}
+	
+	public List<User> findUsers(){
+		return this.userRepository.getAll();
 	}
 }
 
---
+++ src/test/java/internship/issuetracker/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package internship.issuetracker.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserServiceTest {
+
+	private UserService userService;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
