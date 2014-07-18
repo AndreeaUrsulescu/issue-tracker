5fff02270b471b5603dd37c6a161d44902610dfe
CristianNeamtu <cristian.neamtu.93@gmail.com>
2014-07-18 15:08:31 +0300

    Added unit tests for Issue entity

--- src/main/java/internship/issuetracker/entities/Issue.java
+++ src/main/java/internship/issuetracker/entities/Issue.java
@@ -20,109 +20,108 @@ import javax.validation.constraints.Size;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-
 @SuppressWarnings("serial")
 @NamedQueries({
-		@NamedQuery(name = Issue.FIND_TITLE, query = "select a from Issue a where title = :title"),
-		@NamedQuery(name = Issue.FIND_DATE, query = "select a from Issue a where a.updateDate= :updateDate") })
+	@NamedQuery(name = Issue.FIND_TITLE, query = "select a from Issue a where title = :title"),
+	@NamedQuery(name = Issue.FIND_DATE, query = "select a from Issue a where a.updateDate= :updateDate") })
 @Entity
 @Table(name = "Issues")
-public class Issue implements Serializable{
+public class Issue implements Serializable {
 
-	public static final String FIND_TITLE = "Issue.findTitle";
-	public static final String FIND_DATE = "Issue.findDate";
+    public static final String FIND_TITLE = "Issue.findTitle";
+    public static final String FIND_DATE = "Issue.findDate";
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "id_owner", nullable = false)
-	User owner;
-	
-	@Column(name = "title", nullable = false)
-	@Size(min=5,max=50)
-	String title;
+    @ManyToOne
+    @JoinColumn(name = "id_owner", nullable = false)
+    User owner;
 
-	@Column(name = "content")
-	@Size(max=150)
-	String content;
+    @Column(name = "title", nullable = false)
+    @Size(min = 5, max = 50)
+    String title;
 
-	@Column(name = "update_date", nullable = false)
-	Date updateDate;
+    @Column(name = "content")
+    @Size(max = 150)
+    String content;
 
-	@Column(name = "state", nullable = false)
-	State state;
+    @Column(name = "update_date", nullable = false)
+    Date updateDate;
 
-	public Issue() {
-		state = State.New;
+    @Column(name = "state", nullable = false)
+    State state;
+
+    public Issue() {
+	state = State.New;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public User getOwner() {
+	return owner;
+    }
+
+    public void setOwner(User owner) {
+	this.owner = owner;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+
+    public String getContent() {
+	return content;
+    }
+
+    public void setContent(String content) {
+	this.content = content;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public State getState() {
+	return state;
+    }
+
+    public void setState(State state) {
+	this.state = state;
+    }
+
+    @Override
+    public int hashCode() {
+	return new HashCodeBuilder().append(title).append(owner)
+		.append(updateDate).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (obj instanceof Issue) {
+	    if (this == obj) {
+		Issue issue = (Issue) obj;
+		return new EqualsBuilder().append(this.title, issue.title)
+			.append(this.updateDate, issue.updateDate)
+			.append(this.owner, issue.owner)
+			.append(this.state, issue.state).isEquals();
+	    }
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public User getOwner() {
-		return owner;
-	}
-
-	public void setOwner(User owner) {
-		this.owner = owner;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Date getUpdate_date() {
-		return updateDate;
-	}
-
-	public void setUpdate_date(Date update_date) {
-		this.updateDate = update_date;
-	}
-
-	public State getState() {
-		return state;
-	}
-
-	public void setState(State state) {
-		this.state = state;
-	}
-
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(title).append(owner)
-				.append(updateDate).toHashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Issue) {
-			if (this == obj) {
-				Issue issue = (Issue) obj;
-				return new EqualsBuilder().append(this.title, issue.title)
-						.append(this.updateDate, issue.updateDate)
-						.append(this.owner, issue.owner)
-						.append(this.state, issue.state).isEquals();
-			}
-		}
-		return false;
-	}
+	return false;
+    }
 }
