193ed57a265a5d156a4ec543473b368c6aa33b2b
CristianNeamtu <cristian.neamtu.93@gmail.com>
2014-07-18 16:59:15 +0300

    Almost done validation

--- src/main/webapp/resources/assets/javascript/script.js
+++ src/main/webapp/resources/assets/javascript/script.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
 	
-//	$(".form-group input").keyup(function (){
-//		var value = $(this).val();
-//		$(this).parent().find(".error").text(" ");
-//		var field = $(this).parent().find("label").val();
-//		console.log(field);
-//		if(value.length == 0){
-//			$(this).parent().find(".error").text(field + "'s text area can't be empty.");
-//		}
-//		console.log(value);
-//		
-////		if()
-//	});
+	$("#emailAdress").keyup(function(){
+		var value  = $("#emailAdress").val();
+		
+		var email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		
+		var validUsername = value.match(email_regex);
+		
+		$("#emailAdress").parent().find("span").text(" ");
+		
+		if(value.length == 0){
+			$("#emailAdress").parent().find("span").text("Your email adress can not be empty, an input is required.");
+		} else
+		if(validUsername == null){
+			$("#emailAdress").parent().find("span").text("Invalid email adress");
+		}
+	});
 	
 	$("#username").keyup(function(){
 		var value  = $("#username").val();
 		
-		var usernameRegex = '/^[a-zA-Z]{5,12}$/';
+		var usernameRegex = /^[a-zA-Z]+$/;
 		
-		var validationRegex = value.match(usernameRegex);
+		var validUsername = value.match(usernameRegex);
 		
 		$("#username").parent().find("span").text(" ");
 		
 		if(value.length == 0){
 			$("#username").parent().find("span").text("Your username can not be empty, an input is required.");
-		}
-		
-		if((value.length!=0) && (value.length<5 || value.length>12)){
-
+		} else
+		if(value.length<5 || value.length>12){
 			$("#username").parent().find("span").text("Your username must be between 5-12 characters long.");
-			
-		} else{
-			if(value.match(validationRegex)){
-				$("#username").parent().find("span").text("You can only use alphabetical characters.");
-			}
+		} else
+		if(validUsername == null){
+			$("#username").parent().find("span").text("You can only use alphabetical characters.");
 		}
 	});
 	
@@ -53,5 +53,19 @@ $(document).ready(function(){
 			$("#password").parent().find("span").text("Your password must be at least 5 characters long.");
 		}
 		
+	});
+	
+	$("#passwordConfirm").keyup(function(){
+		
+		var password = $("#password").val();
+		var confirm = $("#passwordConfirm").val();
+		
+		$("#passwordConfirm").parent().find("span").text(" ");
+		
+		if(password !== confirm){
+			
+			$("#passwordConfirm").parent().find("span").text("Passwords do not match.");
+			
+		}
 	});
 });
