291b49164ea7a6f29d14557e873c0fba5bbcef7f
CristianNeamtu <cristian.neamtu.93@gmail.com>
2014-07-18 12:30:55 +0300

    Implemented IssueRepository

---
+++ src/main/java/internship/issuetracker/repository/IssueRepository.java
@@ -0,0 +1,33 @@
+package internship.issuetracker.repository;
+
+import internship.issuetracker.entities.Issue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class IssueRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public void create(Issue issue){
+	em.persist(issue);
+    }
+    
+    public void update(Issue issue){
+	em.merge(issue);
+    }
+    
+    public Issue find(Long id){
+	TypedQuery<Issue> query = em.createQuery("select issue from Issue issue where issue.id = ?1",Issue.class);
+	Issue result = query.setParameter(1, id).getSingleResult();
+	return result;
+	
+    }
+}
