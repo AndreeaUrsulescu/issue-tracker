0839b9657216f5f5c9c80f41dd81d2ad75296371
unknown <aursulescu@EN61012.endava.net>
2014-07-21 12:24:36 +0300

    Update IssueController for update issue

--- src/main/java/internship/issuetracker/controller/IssueController.java
+++ src/main/java/internship/issuetracker/controller/IssueController.java
@@ -13,16 +13,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/createIssue")
 public class IssueController {
 	@Autowired
 	private IssueService issueService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = {"/createIssue"}, method = RequestMethod.GET)
 	public String createIssuePage(Model model) {
 		Issue issue = new Issue();
 		
@@ -41,13 +41,28 @@ public class IssueController {
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(value = {"/createIssue"}, method = RequestMethod.POST)
 	public String createIssuePage(@Valid Issue issue,
 			BindingResult bindingResult) {
 		if (bindingResult.hasErrors())
 			return "createIssue";
 
 		issueService.addIssue(issue);
+		return "redirect:/issues";
+	}
+	
+	@RequestMapping(value = "/issues/{id}", method = RequestMethod.PUT)
+	public String updateIssue(@PathVariable Long id, @Valid Issue issue, BindingResult bindingResult, Model model) {
+		Issue initialIssue;
+		
+		if (bindingResult.hasErrors())
+		{
+			initialIssue = issueService.getIssue(id);
+			model.addAttribute(initialIssue);
+			return "viewIssue";
+		}
+		
+		issueService.updateIssue(issue);
 		return "redirect:/issues";
 	}
 }
