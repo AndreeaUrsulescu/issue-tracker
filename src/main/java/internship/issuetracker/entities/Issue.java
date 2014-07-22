e1aea7fa4dc2dd582311a92334569cbdd06afef9
astirbat <Alin.Stirbat@endava.com>
2014-07-22 09:39:18 +0300

    edit issue

--- src/main/java/internship/issuetracker/entities/Issue.java
+++ src/main/java/internship/issuetracker/entities/Issue.java
@@ -26,11 +26,9 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @SuppressWarnings("serial")
 @NamedQueries({
-		@NamedQuery(name = Issue.FIND_BY_TITLE, query = "select a from Issue a where lower(title) = lower(:title) order by updateDate"),
+		@NamedQuery(name = Issue.FIND_BY_TITLE, query = "select a from Issue a where lower(title) = lower(:title) order by updateDate DESC"),
 		@NamedQuery(name = Issue.FIND_BY_DATE, query = "select a from Issue a where a.updateDate= :updateDate"),
 		@NamedQuery(name = Issue.FIND_BY_ID , query = "select a from Issue a where id = :id"),
-		@NamedQuery(name = Issue.FIND_ALL , query = "select a from Issue a order by updateDate")
-		
 		@NamedQuery(name = Issue.FIND_ALL, query = "select a from Issue a order by a.updateDate DESC")
 		})
 @Entity
@@ -42,7 +40,6 @@ public class Issue implements Serializable {
 	public static final String FIND_BY_ID = "Issue.findByID";
 	public static final String FIND_ALL = "Issue.findAll";
 	
-	public static final String FIND_ALL = "Issue.findALL";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
