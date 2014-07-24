042e9b6c974f227e360f4c81cfeb58e563f687ad
astirbat <Alin.Stirbat@endava.com>
2014-07-24 15:08:41 +0300

    solver conflicts

--- src/main/java/internship/issuetracker/service/SearchService.java
+++ src/main/java/internship/issuetracker/service/SearchService.java
@@ -13,32 +13,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SearchService {
-    
-    @Autowired
-    private SearchRepository searchRepository;
-    
-    public int numberOfIssuesByTitle(String title) {
-	return searchRepository.numberOfIssuesByTitle(title);
-	
-    }
-    public List<Issue> findOrderedIssuesByTitle(String title, int currentPage) {
-	return searchRepository.findOrderedIssuesByTitle(title, currentPage);
-    }
-    
-    public int numberOfIssuesByState(State state) {
-	return searchRepository.numberOfIssuesByState(state);
-	
-    }
-    public List<Issue> findOrderedIssuesByState(State state, int currentPage) {
-	return searchRepository.findOrderedIssuesByState(state, currentPage);
-    }
-	public int numberOfIssuesByContent(String content) {
-		return searchRepository.numberOfIssuesByContent(content);
+
+	@Autowired
+	private SearchRepository searchRepository;
+
+	public int numberOfIssuesByTitle(String title) {
+		return searchRepository.numberOfIssuesByTitle(title);
+
 	}
 
-	public List<IssuePojo> findOrderedIssues(String title, int currentPage) {
+	public List<IssuePojo> findOrderedIssuesByTitle(String title,
+			int currentPage) {
 
-		List<Issue> issuesListEntity = searchRepository.findOrderedIssues(title, currentPage);
+		List<Issue> issuesListEntity = searchRepository
+				.findOrderedIssuesByTitle(title, currentPage);
 		List<IssuePojo> issuesListPojo = new ArrayList<IssuePojo>();
 
 		for (int index = 0; index < issuesListEntity.size(); index++) {
@@ -51,5 +39,38 @@ public class SearchService {
 
 		}
 		return issuesListPojo;
+	}
+
+	public int numberOfIssuesByState(State state) {
+		return searchRepository.numberOfIssuesByState(state);
+
+	}
+
+	public List<IssuePojo> findOrderedIssuesByState(State state, int currentPage) {
+		List<Issue> issuesListEntity = searchRepository
+				.findOrderedIssuesByState(state, currentPage);
+		List<IssuePojo> issuesListPojo = new ArrayList<IssuePojo>();
+
+		for (int index = 0; index < issuesListEntity.size(); index++) {
+			Issue issueEntity = issuesListEntity.get(index);
+			IssuePojo issuePojo = new IssuePojo(issueEntity.getId(),
+					issueEntity.getOwner().getUserName(),
+					issueEntity.getTitle(), issueEntity.getContent(),
+					issueEntity.getUpdateDate(), issueEntity.getState());
+			issuesListPojo.add(index, issuePojo);
+
+		}
+		return issuesListPojo;
+
+	}
+
+	public int numberOfIssuesByContent(String content) {
+		return searchRepository.numberOfIssuesByContent(content);
+	}
+
+	public List<IssuePojo> findOrderedIssuesByContent(String content,
+			int currentPage) {
+		return searchRepository
+				.findOrderedIssuesByContent(content, currentPage);
 	}
 }
