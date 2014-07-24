a9ed5f9184c02d99964551565ccb8fb878a773a9
unknown <aursulescu@EN61012.endava.net>
2014-07-24 14:26:07 +0300

    SearchByContent - service & repository

--- src/main/java/internship/issuetracker/repository/SearchRepository.java
+++ src/main/java/internship/issuetracker/repository/SearchRepository.java
@@ -1,7 +1,9 @@
 package internship.issuetracker.repository;
 
 import internship.issuetracker.entities.Issue;
+import internship.issuetracker.pojo.IssuePojo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -11,6 +13,11 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class SearchRepository {
 
     public static int itemsPerPage = 10;
@@ -22,7 +29,7 @@ public class SearchRepository {
 	CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 	CriteriaQuery<Issue> criteriaQuery = criteriaBuilder.createQuery(Issue.class);
 	Root<Issue> root = criteriaQuery.from(Issue.class);
-	TitleFilter titleFilter = new TitleFilter("title");
+	TitleFilter titleFilter = new TitleFilter(title);
 	criteriaQuery.where(titleFilter.buildPredicate(criteriaQuery,criteriaBuilder, root));
 	TypedQuery<Issue> query = em.createQuery(criteriaQuery);
 	return query.getResultList().size();
@@ -32,12 +39,32 @@ public class SearchRepository {
 	CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 	CriteriaQuery<Issue> criteriaQuery = criteriaBuilder.createQuery(Issue.class);
 	Root<Issue> root = criteriaQuery.from(Issue.class);
-	TitleFilter titleFilter = new TitleFilter("title");
+	TitleFilter titleFilter = new TitleFilter(title);
 	criteriaQuery.where(titleFilter.buildPredicate(criteriaQuery,criteriaBuilder, root));
 	TypedQuery<Issue> query = em.createQuery(criteriaQuery);
 	query.setMaxResults(itemsPerPage);
 	query.setFirstResult((currentPage - 1) * itemsPerPage);
 	return query.getResultList();
+    }
+    
+    public List<IssuePojo> findIssuesByContent(String content, int currentPage) {
+    	CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+    	CriteriaQuery<Issue> criteriaQuery = criteriaBuilder.createQuery(Issue.class);
+    	Root<Issue> root = criteriaQuery.from(Issue.class);
+    	ContentFilter contentFilter = new ContentFilter(content);
+    	criteriaQuery.where(contentFilter.buildPredicate(criteriaQuery, criteriaBuilder, root));
+    	TypedQuery<Issue> query = em.createQuery(criteriaQuery);
+    	query.setMaxResults(itemsPerPage);
+    	query.setFirstResult((currentPage - 1) * itemsPerPage);
+    	List<Issue> issues = query.getResultList();
+    	List<IssuePojo> pojoIssues = new ArrayList<IssuePojo>();
+    	
+    	for (Issue issue : issues) {
+    		IssuePojo pojo = new IssuePojo(issue.getId(), issue.getOwner().getUserName(), issue.getTitle(), issue.getContent(), issue.getUpdateDate(), issue.getState());
+    		pojoIssues.add(pojo);
+    	}
+    	
+    	return pojoIssues;
     }
     
 }
--- src/main/java/internship/issuetracker/service/SearchService.java
+++ src/main/java/internship/issuetracker/service/SearchService.java
@@ -3,10 +3,13 @@ package internship.issuetracker.service;
 import java.util.List;
 
 import internship.issuetracker.entities.Issue;
+import internship.issuetracker.pojo.IssuePojo;
 import internship.issuetracker.repository.SearchRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SearchService {
     
     @Autowired
@@ -18,6 +21,10 @@ public class SearchService {
     }
     public List<Issue> findOrderedIssues(String title, int currentPage) {
 	return searchRepository.findOrderedIssues(title, currentPage);
+    }
+    
+    public List<IssuePojo> findIssuesByContent(String content, int currentPage) {
+    	return searchRepository.findIssuesByContent(content, currentPage);
     }
 
 }
