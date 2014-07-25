067e28b61f83ec684339446e7c7e4bdf951b7b1a
astirbat <Alin.Stirbat@endava.com>
2014-07-25 14:59:11 +0300

    test up

--- src/test/java/internship/issuetracker/repository/TitleFIlterTest.java
+++ src/test/java/internship/issuetracker/repository/TitleFIlterTest.java
@@ -7,83 +7,78 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.validation.constraints.AssertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/datasource/h2.xml", "classpath:config/application-context.xml" })
 public class TitleFIlterTest {
 
-    EntityManager em;
-    private IssueRepository issueRepository;
-    private UserRepository userRepository;
-    private User user;
-    static int count = 65;
-    static int ucount = 65;
-    
-    public Issue createIssue(String title) {
-	Issue issue = new Issue();
-	issue.setContent("contentx");
-	issue.setTitle(title);
-	issue.setOwner(user);
-	issue.setUpdateDate(new Date());
-	count++;
-	return issue;
-    }
+	@PersistenceContext
+	EntityManager em;
+	@Autowired
+	private IssueRepository issueRepository;
+	@Autowired
+	private UserRepository userRepository;
+	private User user;
+	static int count = 65;
+	static int ucount = 65;
 
-    @Before
-    public void setUp() {
-	@SuppressWarnings("resource")
-	ApplicationContext context = new ClassPathXmlApplicationContext(
-		"config/datasource/h2.xml", "config/application-context.xml");
-	EntityManagerFactory emf = (EntityManagerFactory) context.getBean("entityManagerFactory");
-	em = emf.createEntityManager();
-	issueRepository = context.getBean(IssueRepository.class);
-	userRepository = context.getBean(UserRepository.class);
-	user = new User();
-	user.setUserName("userx"+(char)ucount);
-	user.setEmail("email@end.com");
-	user.setPassword("parola");
-	userRepository.create(user);
-	ucount++;
-    }
-
-    @Test
-    public void test() {
-	Issue issue = createIssue("title");
-	issueRepository.create(issue);
-	issue = createIssue("context");
-	issueRepository.create(issue);
-	issue = createIssue("contact");
-	issueRepository.create(issue);
-	issue = createIssue("titlefsda");
-	issueRepository.create(issue);
-	
-	CriteriaBuilder cb = em.getCriteriaBuilder();
-	CriteriaQuery<Issue> cq = cb.createQuery(Issue.class);
-	Root<Issue> root = cq.from(Issue.class);
-	TitleFilter tf = new TitleFilter("");
-	cq.where(tf.buildPredicate(cq, cb, root));
-	cq.orderBy(cb.desc(root.get("updateDate")),cb.desc(root.get("id")));
-	TypedQuery<Issue> tq = em.createQuery(cq);
-	List<Issue> list = tq.getResultList();
-	
-	for (Issue issue2 : list) {
-		
-		System.out.println(issue2.getUpdateDate()+" "+issue2.getId());
+	public Issue createIssue(String title) {
+		Issue issue = new Issue();
+		issue.setContent("contentx");
+		issue.setTitle(title);
+		issue.setOwner(user);
+		issue.setUpdateDate(new Date());
+		count++;
+		return issue;
 	}
-	assert(list.size() == 1);
-    }
+
+	@Before
+	public void setUp() {
+		user = new User();
+		user.setUserName("userx" + (char) ucount);
+		user.setEmail("email@end.com");
+		user.setPassword("parola");
+		userRepository.create(user);
+		ucount++;
+	}
+
+	@Test
+	public void test() {
+		Issue issue = createIssue("title");
+		issueRepository.create(issue);
+		issue = createIssue("context");
+		issueRepository.create(issue);
+		issue = createIssue("contact");
+		issueRepository.create(issue);
+		issue = createIssue("titlefsda");
+		issueRepository.create(issue);
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Issue> cq = cb.createQuery(Issue.class);
+		Root<Issue> root = cq.from(Issue.class);
+		TitleFilter tf = new TitleFilter("");
+		cq.where(tf.buildPredicate(cq, cb, root));
+		cq.orderBy(cb.desc(root.get("updateDate")), cb.desc(root.get("id")));
+		TypedQuery<Issue> tq = em.createQuery(cq);
+		List<Issue> list = tq.getResultList();
+
+		for (Issue issue2 : list) {
+
+			System.out.println(issue2.getUpdateDate() + " " + issue2.getId());
+		}
+		assert (list.size() == 1);
+	}
 
 }
