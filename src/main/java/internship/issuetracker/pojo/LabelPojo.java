584181d695a00c42134642100dccd65e33c29260
CiprianTalmacel <cprn_talmacel@yahoo.com>
2014-07-28 14:52:43 +0300

    2:52

---
+++ src/main/java/internship/issuetracker/pojo/LabelPojo.java
@@ -0,0 +1,14 @@
+package internship.issuetracker.pojo;
+
+public class LabelPojo {
+	
+	String labelName;
+
+	public String getLabelName() {
+		return labelName;
+	}
+
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+}
