6a21fc3062abd347e151d652defe4301e12da7cb
CristianNeamtu <cristian.neamtu.93@gmail.com>
2014-08-04 16:44:39 +0300

    Bugfix: Web Browser back button

--- src/main/java/internship/issuetracker/controller/LoginController.java
+++ src/main/java/internship/issuetracker/controller/LoginController.java
@@ -1,5 +1,12 @@
 package internship.issuetracker.controller;
 
+import internship.issuetracker.entities.User;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
+	private static final Logger log = Logger.getLogger(LoginController.class
+			.getName());
+
 	@RequestMapping(value = { "/login" }, method = RequestMethod.GET)
-	public String login(ModelMap model) {
+	public String login(HttpServletRequest request, ModelMap model) {
+
+		boolean isLoggedIn = false;
+		User user = (User) request.getSession().getAttribute("user");
+		log.log(Level.INFO, "checking for logged in:" + user);
+		isLoggedIn = user != null;
+		if (isLoggedIn) {
+			return "redirect:/issues";
+		}
 		return "login";
 	}
 
