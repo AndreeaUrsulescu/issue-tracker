57bd79728bd818ec897e0471cf3c31570ad7332c
CiprianTalmacel <cprn_talmacel@yahoo.com>
2014-08-04 17:15:02 +0300

    Signed-off-by: CiprianTalmacel <cprn_talmacel@yahoo.com>

--- src/main/java/internship/issuetracker/controller/RestIssueController.java
+++ src/main/java/internship/issuetracker/controller/RestIssueController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/issues")
+@RequestMapping( "/issues")
 public class RestIssueController {
 
 	@Autowired
