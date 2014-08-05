633d414da9397a1a7c5f1cdae1ed288866f9cdca
astirbat <Alin.Stirbat@endava.com>
2014-08-05 17:30:08 +0300

    update login valid

--- src/main/webapp/resources/assets/javascript/login-validator.js
+++ src/main/webapp/resources/assets/javascript/login-validator.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$("#username").focus();
+	$("#usernameX").focus();
 	
 	$(".registerError").hide();
 	
@@ -9,49 +9,47 @@ $(document).ready(function(){
 	}
 	
 	function validateUserName(){
-		var value  = $("#username").val();
+		var value  = $("#usernameX").val();
 		
 		var usernameRegex = /^[a-zA-Z]+$/;
 		
 		var validUsername = value.match(usernameRegex);
 		
-		$("#username").parent().find("span").text(" ");
+		$("#usernameX").parent().find("span").text(" ");
 		
 		if(value.length == 0){
-			$("#username").parent().find("span").text("Your username can not be empty, an input is required.");
+			$("#usernameX").parent().find("span").text("Your username can not be empty, an input is required.");
 			return false;
 		} else
 		if(value.length<5 || value.length>12){
-			$("#username").parent().find("span").text("Your username must be between 5-12 characters long.");
+			$("#usernameX").parent().find("span").text("Your username must be between 5-12 characters long.");
 			return false;
 		} else
 		if(validUsername == null){
-			$("#username").parent().find("span").text("You can only use alphabetical characters.");
+			$("#usernameX").parent().find("span").text("You can only use alphabetical characters.");
 			return false;
 		}
 		return true;
 	};
 	
 	function validatePassword(){
-		var value = $("#password").val();
-		$("#password").parent().find("span").text(" ");
+		var value = $("#passwordX").val();
+		$("#passwordX").parent().find("span").text(" ");
 		if(value.length == 0){
-			$("#password").parent().find("span").text("Your password can not be empty, an input is required.");
+			$("#passwordX").parent().find("span").text("Your password can not be empty, an input is required.");
 			return false;
 		}
 		if((value.length!=0) && value.length<5){
-			$("#password").parent().find("span").text("Your password must be at least 5 characters long.");
+			$("#passwordX").parent().find("span").text("Your password must be at least 5 characters long.");
 			return false;
 		}
 		return true;
 	};
 	
-	$("#username").focusout(validateUserName);
-	$("#password").focusout(validatePassword);
-	
+
 	$("#login-form").submit(function(event){
 
-	    var isValid = validatePassword() && validateUserName();
+	    var isValid =  validateUserName() && validatePassword() ;
 
 	    if (!isValid) {
 	        event.preventDefault();
