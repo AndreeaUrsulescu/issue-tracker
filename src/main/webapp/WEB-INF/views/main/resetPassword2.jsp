9f9fa954d9eee80d8049e2c360318a8f21569a82
astirbat <Alin.Stirbat@endava.com>
2014-08-05 16:47:46 +0300

    reset password done

--- src/main/webapp/WEB-INF/views/main/resetPassword2.jsp
+++ src/main/webapp/WEB-INF/views/main/resetPassword2.jsp
@@ -1,7 +1,8 @@
 <%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-
+<script src="${pageContext.request.contextPath}/resources/assets/javascript/resetPassword-validation.js"></script>
+			
 <div class="container">
 	<sf:form id="resetPasswordForm" method="POST" role="form"
 		modelAttribute="userName">
---
+++ src/main/webapp/resources/assets/javascript/resetPassword-validation.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	function validatePassword() {
+		var value = $("#password").val();
+		$("#password").parent().find("span").text(" ");
+		if (value.length == 0) {
+			$("#password")
+					.parent()
+					.find("span")
+					.text(
+							"Your password can not be empty, an input is required.");
+			return false;
+		}
+		if ((value.length != 0) && value.length < 5) {
+			$("#password")
+					.parent()
+					.find("span")
+					.text(
+							"Your password must be at least 5 characters long.");
+			return false;
+		}
+		return true;
+	}
+	;
+
+	function validateConfirm() {
+		var password = $("#password").val();
+		var confirm = $("#passwordConfirm").val();
+		$("#passwordConfirm").parent().find("span").text(" ");
+		if (password !== confirm) {
+			$("#passwordConfirm").parent().find("span").text(
+					"Passwords do not match.");
+			return false;
+		}
+		return true;
+	}
+	;
+	
+	
+	$("#password").focusout(validatePassword);
+	$("#passwordConfirm").focusout(validateConfirm);
+$("#resetPasswordForm").submit(
+		function(event) {
+
+			var isValid = validatePassword()
+					&& validateConfirm();
+
+			if (!isValid) {
+				event.preventDefault();
+			}
+		});
+});	
