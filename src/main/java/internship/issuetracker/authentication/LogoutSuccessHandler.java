3fc2c4cfd589d11bc9acc6c13dd635293317a455
CristianNeamtu <cristian.neamtu.93@gmail.com>
2014-08-07 11:15:02 +0300

    Refactored Utils package

--- src/main/java/internship/issuetracker/authentication/LogoutSuccessHandler.java
+++ src/main/java/internship/issuetracker/authentication/LogoutSuccessHandler.java
@@ -13,21 +13,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 public class LogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
-	private static final Logger log = Logger
-			.getLogger(LogoutSuccessHandler.class.getName());
+    private static final Logger log = Logger.getLogger(LogoutSuccessHandler.class.getName());
 
-	public void logout(HttpServletRequest request,
-			HttpServletResponse response, Authentication authentication) {
-		
-	}
-
-	@Override
-	public void onLogoutSuccess(HttpServletRequest request,
-			HttpServletResponse response, Authentication authentication)
-			throws IOException, ServletException {
-		log.log(Level.INFO, "User logged out succesfully");
-		SecurityContextHolder.clearContext();
-		request.getSession().setAttribute("user", null);
-		super.onLogoutSuccess(request, response, null);
-	}
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.log(Level.INFO, "User logged out succesfully");
+        SecurityContextHolder.clearContext();
+        request.getSession().setAttribute("user", null);
+        super.onLogoutSuccess(request, response, null);
+    }
 }
