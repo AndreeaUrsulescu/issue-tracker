32ce7751732fad2e0122192923640f8112f5104e
CiprianTalmacel <cprn_talmacel@yahoo.com>
2014-08-07 11:40:58 +0300

    Some more refactoring of services+tests

--- src/test/java/internship/issuetracker/controller/IssueControllerTest.java
+++ src/test/java/internship/issuetracker/controller/IssueControllerTest.java
@@ -3,7 +3,6 @@ package internship.issuetracker.controller;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
